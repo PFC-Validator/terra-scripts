3120e9e891a1ae4c88c5227f1393e620653a2027
petes-fan-club <petes-fan-club@protonmail.com>
2021-03-28 22:46:26 -0400

    add experimental switch for Colombus. not operational yet

--- validator/validator_run.sh
+++ validator/validator_run.sh
@@ -44,7 +44,10 @@ if ! sha256sum -c validator/go1.15.10.linux-amd64.tar.gz.sum ; then
     exit 1
 fi
 tar xfz ./go1.15.10.linux-amd64.tar.gz
-
+if [ -d "/usr/local/go" ]; 
+then
+    sudo rm -rf /usr/local/go
+fi
 sudo mv go /usr/local
 
 # set up paths for next time
@@ -56,6 +59,7 @@ export PATH=${PATH}:/usr/local/go/bin:${PWD}/go/bin
 # get the code
 git clone https://github.com/terra-project/core/
 cd core
+git pull
 make install
 # terraD binaries are in the right place now
 terrad init ${MONIKER} --chain-id ${CHAIN_ID}
@@ -63,24 +67,46 @@ if [ -f ".terrad/config/genesis.json" ];
 then
     rm -f .terrad/config/genesis.json 
 fi 
-# curl https://columbus-genesis.s3-ap-northeast-1.amazonaws.com/genesis.json > $HOME/.terrad/config/genesis.json
-curl https://raw.githubusercontent.com/terra-project/testnet/master/tequila-0004/genesis.json > $HOME/.terrad/config/genesis.json
-curl https://raw.githubusercontent.com/terra-project/testnet/master/tequila-0004/address.json > $HOME/.terrad/config/address.json
-# curl https://network.terra.dev/addrbook.json > $HOME/.terrad/config/addrbook.json
-curl https://network.terra.dev/testnet/addrbook.json > $HOME/.terrad/config/addrbook.json
+
+case "${CHAIN_ID}" in 
+    "Columbus-4") 
+        echo "Columbia not supported yet"
+        curl https://columbus-genesis.s3-ap-northeast-1.amazonaws.com/genesis.json > $HOME/.terrad/config/genesis.json
+        curl https://network.terra.dev/addrbook.json > $HOME/.terrad/config/addrbook.json
+        # TBD is address.json actually needed?
+
+        ;;
+    "tequila-0004")
+        curl https://raw.githubusercontent.com/terra-project/testnet/master/tequila-0004/genesis.json > $HOME/.terrad/config/genesis.json
+        curl https://raw.githubusercontent.com/terra-project/testnet/master/tequila-0004/address.json > $HOME/.terrad/config/address.json
+        curl https://network.terra.dev/testnet/addrbook.json > $HOME/.terrad/config/addrbook.json
+        ;;
+    *)
+        echo "${CHAIN_ID} not known"
+        exit 1
+    ;;
+esac 
 
 pushd ${HOME}
 cp .terrad/config/config.toml .terrad/config/config.toml.orig
 # sed script to fix indexer line to 'null'
 # sed 's/indexer = \"kv\"/indexer = \"null\"/' < .terrad/config/config.toml.orig > .terrad/config/config.toml.1 
 sed 's/\"data/\"\/mnt\/disks\/data\/terrad\/data/' < .terrad/config/config.toml.orig > .terrad/config/config.toml.1
-sed 's/seeds = \"\"/seeds = \"341f51bf381566dfef0fc345c2aa882cbeebd320@public-seed2.terra.dev:36656\"/' < .terrad/config/config.toml.1 > .terrad/config/config.toml
-
-# Columbia
-# seeds = "20271e0591a7204d72280b87fdaa854f50c55e7e@106.10.59.48:26656,3b1c85b86528d10acc5475cb2c874714a69fde1e@110.234.23.153:26656,49333a4cb195d570ea244dab675a38abf97011d2@13.113.103.57:26656,7f19128de85ced9b62c3947fd2c2db2064462533@52.68.3.126:26656"
+case "${CHAIN_ID}" in 
+    "Columbus-4") 
+        echo "Columbia not supported yet"
+        sed 's/seeds = \"\"/seeds = \"20271e0591a7204d72280b87fdaa854f50c55e7e@106.10.59.48:26656,3b1c85b86528d10acc5475cb2c874714a69fde1e@110.234.23.153:26656,49333a4cb195d570ea244dab675a38abf97011d2@13.113.103.57:26656,7f19128de85ced9b62c3947fd2c2db2064462533@52.68.3.126:26656\"/' < .terrad/config/config.toml.1 > .terrad/config/config.toml
+        ;;
+    "tequila-0004")
+        sed 's/seeds = \"\"/seeds = \"341f51bf381566dfef0fc345c2aa882cbeebd320@public-seed2.terra.dev:36656\"/' < .terrad/config/config.toml.1 > .terrad/config/config.toml
+        ;;
+    *)
+        echo "${CHAIN_ID} not known"
+        exit 1
+    ;;  
+esac
 
 # app.toml
-# minimum-gas-prices = "0.01133uluna,0.15uusd,0.104938usdr,169.77ukrw,428.571umnt,0.125ueur,0.98ucny,16.37ujpy,0.11ugbp,10.88uinr,0.19ucad,0.14uchf,0.19uaud,0.2usgd,4.62uthb"
 cp .terrad/config/app.toml .terrad/config/app.toml.orig
 sed 's/minimum-gas-prices = \"\"/minimum-gas-prices = \"0.01133uluna,0.15uusd,0.104938usdr,169.77ukrw,428.571umnt,0.125ueur,0.98ucny,16.37ujpy,0.11ugbp,10.88uinr,0.19ucad,0.14uchf,0.19uaud,0.2usgd,4.62uthb\"/' < ${HOME}/.terrad/config/app.toml.orig > ${HOME}/.terrad/config/app.toml
 
@@ -88,18 +114,38 @@ popd
 terracli config node http://127.0.0.1:26657
 terracli config chain-id ${CHAIN_ID}
 
-#
-#syncfile=$( curl https://terra.quicksync.io/sync.json|jq -r ".[]| select(.network==\"pruned\")|.file" |grep columbus-4)
-syncfile=tequila-4.20210215.tar.lz4
-cd /mnt/disks/data/terrad 
-aria2c --continue -x5 https://get.quicksync.io/${syncfile} -o sync.lz4 
-if ! sha256sum -c ${HOME}/validator/sync.lz4.sum ; then
-    echo "Tequilla Quicksync file download did not match checksum"
-    exit 1
-fi
-# lz4 -d sync.lz4 |tar xf -
-tar --use-compress-program=lz4 -xf sync.lz4
-mv ${HOME}/.terrad/data/priv_validator_state.json /mnt/disks/data/terrad/data
+
+pushd /mnt/disks/data/terrad 
+case "${CHAIN_ID}" in 
+    "Columbus-4") 
+        echo "Columbia not supported yet"
+        syncfile=$( curl https://terra.quicksync.io/sync.json|jq -r ".[]| select(.network==\"pruned\")|.file" |grep columbus-4)
+        echo "syncfile = ${syncfile}"
+        aria2c  -x5 https://get.quicksync.io/${syncfile}
+        curl -O  https://get.quicksync.io/${syncfile}.checksum
+        curl -s https://lcd.terra.dev/txs/`curl -s https://get.quicksync.io/$FILENAME.hash`|jq -r '.tx.value.memo'|sha512sum -c
+        ${HOME}/validator/checksum.sh $FILENAME
+        echo "exit code $?"
+        echo "waiting..."
+        read
+        ;;
+    "tequila-0004")
+        syncfile=tequila-4.20210215.tar.lz4
+        aria2c -x5 https://get.quicksync.io/${syncfile} -o sync.lz4 
+        if ! sha256sum -c ${HOME}/validator/sync.lz4.sum ; then
+            echo "Tequilla Quicksync file download did not match checksum"
+            exit 1
+        fi
+        tar --use-compress-program=lz4 -xf sync.lz4
+        ;;
+    *)
+        echo "${CHAIN_ID} not known"
+        exit 1
+    ;;  
+esac
+
+exit 0
+#mv ${HOME}/.terrad/data/priv_validator_state.json /mnt/disks/data/terrad/data
 # everything is in place ..
 # lighting up daemons
 sudo systemctl daemon-reload
