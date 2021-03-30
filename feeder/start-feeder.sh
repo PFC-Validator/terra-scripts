acc063a0070c346789f8c74ebe93340ad382a8b6
petes-fan-club <petes-fan-club@protonmail.com>
2021-03-30 08:21:29 -0400

    use main LCD

--- feeder/start-feeder.sh
+++ feeder/start-feeder.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 cd /home/user/oracle-feeder/feeder
 source /home/user/oracle_pub_key.env 
-#operatorAddr="${ORACLE_PUB_KEY}"
-	   #--lcd https://lcd.terra.dev \
+
+#	   --lcd http://validator-01:1317 \
 npm start vote -- \
 	   --source http://price-server-01:8532/latest \
-	   --lcd http://validator-01:1317 \
+	   --lcd https://lcd.terra.dev \
 	   --chain-id "${CHAIN_ID}" \
 	--denoms sdr,krw,usd,mnt,eur,cny,jpy,gbp,inr,cad,chf,hkd,aud,sgd \
 	--validator "${VALIDATOR_KEY}" \
