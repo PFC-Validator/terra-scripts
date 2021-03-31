64bc4ae4585fc0d23172ea66b5e403314074e3f4
petes-fan-club <petes-fan-club@protonmail.com>
2021-03-31 17:40:58 -0400

    display what default project is. don't change it

--- remote-scripts/create_vms.sh
+++ remote-scripts/create_vms.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 source settings.default
 source settings.private
-echo "Using Project ID ${PROJECT_ID} to create VMs in zone ${DEFAULT_ZONE}"
+current_project= $(gcloud config list core/project 2>/dev/null|grep project|cut -d= -f2-)
+echo "Using Project ID ${current_project} to create VMs in zone ${DEFAULT_ZONE}"
 echo "waiting for confirmation. <enter> to continue, ^C to abort"
 read
 
