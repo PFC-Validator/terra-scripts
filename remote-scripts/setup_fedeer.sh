722e4d001c86c44cdadaaf589d61c10c62586a5d
petes-fan-club <petes-fan-club@protonmail.com>
2021-04-07 10:55:33 -0400

    connect the oracle/feeder with the validator

--- remote-scripts/setup_fedeer.sh
+++ remote-scripts/setup_fedeer.sh
@@ -2,7 +2,8 @@
 source settings.default
 source settings.private
 #echo "type in your wallet password"
-VALIDATOR_KEY=$(terracli keys show validator --bech=val|grep address|cut -d: -f2)
+VALIDATOR_KEY=$(terracli keys show validator --bech=val -a ) 
+ORACLE_KEY=$( terracli keys show oracle -a) 
 key_exists=$(gcloud secrets list --filter ORACLE_PASSWORD --format=json)
 if [ "$key_exists" == "[]" ];
 then
@@ -10,5 +11,7 @@ then
 else
     printf "${ORACLE_PASSWORD}" | gcloud secrets versions add ORACLE_PASSWORD --data-file=-
 fi
+#terracli tx oracle set-feeder <feeder-address> --from=<validator>
+terracli tx oracle set-feeder ${ORACLE_KEY} --from=validator --fees="30000uluna"
 gcloud compute scp --scp-flag=-r ../feeder/ user@feeder-01: --ssh-key-file=./setup_key_private
 gcloud compute ssh user@feeder-01 --ssh-key-file=./setup_key_private --  ./feeder/feeder_run.sh ${CHAIN_ID} ${VALIDATOR_KEY} 
