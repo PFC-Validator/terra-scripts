a70f960719ed6ccaa3c75854159cea046c475a9e
petes-fan-club <petes-fan-club@protonmail.com>
2021-05-29 08:18:34 -0400

    add DKK/NOK scandanavian currencies

--- price-server/price-server_run.sh
+++ price-server/price-server_run.sh
@@ -38,7 +38,7 @@ npm install
 # Copy sample config file
 # TODO SED
 sed "0,/apiKey:/{s/apiKey: ''/apiKey: '${CURRENCYLAYER_APIKEY}'/}" < ./config/default-sample.js > ./config/default.js.1
-# sed "s/'KRW\/THB',/'KRW\/TBH','KRW\/SEK',/" < ./config/default.js.1 > ./config/default.js
+# sed "s/'KRW\/THB',/'KRW\/TBH',',/'KRW\/NOK',',/'KRW\/DKK','KRW\/SEK',/" < ./config/default.js.1 > ./config/default.js
 popd
 mv price-server/price-server-start.sh oracle-feeder/price-server
 
