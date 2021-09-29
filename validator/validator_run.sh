a6124ac8ae599a7ccca1d39ae527e8d0ab3e2e91
petes-fan-club <petes-fan-club@protonmail.com>
2021-09-29 09:25:55 -0400

    getting ready for col5

--- validator/validator_run.sh
+++ validator/validator_run.sh
@@ -22,11 +22,12 @@ sudo cp /tmp/fstab /etc/fstab
 sudo cp validator/limits.terrad /etc/security/limits.d/terrad.conf
 # install the service definitions
 case "${CHAIN_ID}" in 
-    "bombay-9")
+    "bombay-12")
 	sudo cp validator/terrad.service /etc/systemd/system/
 	;;
     *)
-	sudo cp validator/*.service /etc/systemd/system/
+	sudo cp validator/terrad.service /etc/systemd/system/
+	#sudo cp validator/*.service /etc/systemd/system/
 	;;
 esac
 
@@ -45,14 +46,14 @@ sudo apt-get install -y google-fluentd
 sudo apt-get install -y google-fluentd-catch-all-config 
 sudo service google-fluentd start
 # GO .. as we're building it from source.
-curl -LO https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
-# tar xfz ./go1.16.2.linux-amd64.tar.gz
-#curl -LO https://golang.org/dl/go1.15.12.linux-amd64.tar.gz
-if ! sha256sum -c validator/go1.16.7.linux-amd64.tar.gz.sum ; then
+
+curl -LO https://golang.org/dl/go1.16.8.linux-amd64.tar.gz
+
+if ! sha256sum -c validator/go1.16.8.linux-amd64.tar.gz.sum ; then
     echo "GO download did not match checksum"
     exit 1
 fi
-tar xfz ./go1.16.7.linux-amd64.tar.gz
+tar xfz ./go1.16.8.linux-amd64.tar.gz
 if [ -d "/usr/local/go" ]; 
 then
     sudo rm -rf /usr/local/go
@@ -66,15 +67,15 @@ export GOPATH=${HOME}/go
 export PATH=${PATH}:/usr/local/go/bin:${PWD}/go/bin
 
 # get the code
-# TBD: currently pulls 'master', should it pull v0.4.6 ?
-git clone https://github.com/terra-project/core/
+
+git clone https://github.com/terra-money/core/
 cd core
 case "${CHAIN_ID}" in 
-    "bombay-9")
-	git checkout v0.5.0
+    "bombay-12")
+	git checkout v0.5.5-oracle
 	;;
     *)
-	git checkout v0.4.6
+	git checkout v0.5.5-oracle
 	;;
 esac
 
@@ -85,26 +86,23 @@ esac
 make install
 # terraD binaries are in the right place now
 terrad init "${MONIKER}" --chain-id ${CHAIN_ID}
-if [ -f ".terrad/config/genesis.json" ];
+if [ -f ".terra/config/genesis.json" ];
 then
-    rm -f .terrad/config/genesis.json 
+    rm -f .terra/config/genesis.json 
 fi 
 
 case "${CHAIN_ID}" in 
-    "Columbus-4") 
-        echo "Columbia not supported yet"
-        curl https://columbus-genesis.s3-ap-northeast-1.amazonaws.com/genesis.json > $HOME/.terrad/config/genesis.json
-        curl https://network.terra.dev/addrbook.json > $HOME/.terrad/config/addrbook.json
+    "columbus-5") 
+        echo "Columbus-5 not supported yet"
+	# TBD
+#        curl https://columbus-genesis.s3-ap-northeast-1.amazonaws.com/genesis.json > $HOME/.terrad/config/genesis.json
+#        curl https://network.terra.dev/addrbook.json > $HOME/.terrad/config/addrbook.json
         # TBD is address.json actually needed?
 
         ;;
-    "tequila-0004")
-        curl https://raw.githubusercontent.com/terra-project/testnet/master/tequila-0004/genesis.json > $HOME/.terrad/config/genesis.json
-        curl https://raw.githubusercontent.com/terra-project/testnet/master/tequila-0004/address.json > $HOME/.terrad/config/address.json
-        curl https://network.terra.dev/testnet/addrbook.json > $HOME/.terrad/config/addrbook.json
-        ;;
-    "bombay-9")
-        curl https://raw.githubusercontent.com/terra-project/testnet/master/bombay-9/genesis.json > $HOME/.terra/config/genesis.json
+    "bombay-12")
+#        curl https://raw.githubusercontent.com/terra-project/testnet/master/bombay-9/genesis.json > $HOME/.terra/config/genesis.json
+	curl https://raw.githubusercontent.com/terra-money/testnet/master/bombay-12/genesis.json > $HOME/.terra/config/genesis.json
 	;;
     *)
         echo "${CHAIN_ID} not known"
@@ -112,11 +110,11 @@ case "${CHAIN_ID}" in
     ;;
 esac 
 case "${CHAIN_ID}" in 
-    "bombay-9")
+    "bombay-12")
 	pushd ${HOME}/.terra
 	;;
     *)
-	pushd ${HOME}/.terrad
+	pushd ${HOME}/.terra
 	;;
 esac
 
@@ -127,14 +125,11 @@ cp ./config/config.toml ./config/config.toml.orig
 # sed 's/indexer = \"kv\"/indexer = \"null\"/' < .terrad/config/config.toml.orig > .terrad/config/config.toml.1 
 sed 's/\"data/\"\/mnt\/disks\/data\/terrad\/data/' < ./config/config.toml.orig > ./config/config.toml.1
 case "${CHAIN_ID}" in 
-    "Columbus-4") 
+    "columbus-5") 
         echo "Columbia not supported yet"
         sed 's/seeds = \"\"/seeds = \"20271e0591a7204d72280b87fdaa854f50c55e7e@106.10.59.48:26656,3b1c85b86528d10acc5475cb2c874714a69fde1e@110.234.23.153:26656,49333a4cb195d570ea244dab675a38abf97011d2@13.113.103.57:26656,7f19128de85ced9b62c3947fd2c2db2064462533@52.68.3.126:26656\"/' < ./config/config.toml.1 > ./config/config.toml
         ;;
-    "tequila-0004")
-        sed 's/seeds = \"\"/seeds = \"341f51bf381566dfef0fc345c2aa882cbeebd320@public-seed2.terra.dev:36656\"/' < ./config/config.toml.1 > ./config/config.toml
-        ;;
-    "bombay-9")
+    "bombay-12")
         sed 's/seeds = \"\"/seeds = \"8eca04192d4d4d7da32149a3daedc4c24b75f4e7@3.34.163.215:26656\"/' < ./config/config.toml.1 > ./config/config.toml
         ;;
     *)
@@ -146,17 +141,18 @@ esac
 # app.toml
 cp ./config/app.toml ./config/app.toml.orig
 sed 's/minimum-gas-prices = \"\"/minimum-gas-prices=\"0.013199uluna,0.38uusd,0.527101uaud,0.481912ucad,0.347244uchf,2.455638ucny,2.402775udkk,0.323109ueur,0.276745ugbp,2.950842uhkd,5450.0uidr,28.085651uinr,41.68797ujpy,443.515327ukrw,1061.675585umnt,3.314161unok,19.0uphp,0.267408usdr,3.314161usek,0.514572usgd,12.581803uthb\"/' < ./config/app.toml.orig > ./config/app.toml
-#minimum-gas-prices = \"0.01133uluna,0.15uusd,0.104938usdr,169.77ukrw,428.571umnt,0.125ueur,0.98ucny,16.37ujpy,0.11ugbp,10.88uinr,0.19ucad,0.14uchf,0.19uaud,0.2usgd,4.62uthb,1.25usek,1.25unok,0.9udkk\"/
+
+
 popd
-case "${CHAIN_ID}" in 
-    "bombay-9")
-	echo "skipping terracli as it isn't installed"
-	;;
-    *)
-	terracli config node http://127.0.0.1:26657
-	terracli config chain-id ${CHAIN_ID}
-	;;
-esac
+#case "${CHAIN_ID}" in 
+#    "bombay-12")
+#	echo "skipping terracli as it isn't installed"
+#	;;
+#    *)
+#	terracli config node http://127.0.0.1:26657
+#	terracli config chain-id ${CHAIN_ID}
+#	;;
+#esac
 
 
 chmod 700 ${HOME}/validator/checksum.sh
@@ -164,41 +160,25 @@ chmod 700 ${HOME}/validator/checksum.sh
 
 pushd /mnt/disks/data/terrad 
 case "${CHAIN_ID}" in 
-    "Columbus-4") 
+    "columbus-5") 
         echo "Columbia not supported yet"
-        syncfile=$( curl https://terra.quicksync.io/sync.json|jq -r ".[]| select(.network==\"pruned\")|.file" |grep columbus-4)
-        echo "syncfile = ${syncfile}"
-        aria2c  -x5 https://get.quicksync.io/${syncfile}
-        curl -O  https://get.quicksync.io/${syncfile}.checksum
-        hash=$(curl -s https://get.quicksync.io/${syncfile}.hash)
-        curl -s https://lcd.terra.dev/txs/${hash}|jq -r '.tx.value.memo'|sha512sum -c
-        ${HOME}/validator/checksum.sh ${syncfile}
+#        syncfile=$( curl https://terra.quicksync.io/sync.json|jq -r ".[]| select(.network==\"pruned\")|.file" |grep columbus-4)
+#        echo "syncfile = ${syncfile}"
+#        aria2c  -x5 https://get.quicksync.io/${syncfile}
+#        curl -O  https://get.quicksync.io/${syncfile}.checksum
+#        hash=$(curl -s https://get.quicksync.io/${syncfile}.hash)
+#        curl -s https://lcd.terra.dev/txs/${hash}|jq -r '.tx.value.memo'|sha512sum -c
+#        ${HOME}/validator/checksum.sh ${syncfile}
 
-        echo "exit code $?"
-        echo "waiting..."
-        read
+#        echo "exit code $?"
+#        echo "waiting..."
+#        read
 
-	mv ${HOME}/.terrad/data ${HOME}/.terrad/data.orig
-	ln -s /mnt/disks/data/terrad/data ${HOME}/.terrad/
+	mv ${HOME}/.terra/data ${HOME}/.terra/data.orig
+	ln -s /mnt/disks/data/terrad/data ${HOME}/.terra/
+	cp ${HOME}/.terra/data.orig/priv_validator_state.json ${HOME}/.terra/data/
         ;;
-    "tequila-0004")
-       # syncfile=tequila-4.20210215.tar.lz4
-        syncfile=$( curl https://terra.quicksync.io/sync.json|jq -r ".[]| select(.network==\"default\")|.file" |grep tequila-4)
-        echo "syncfile = ${syncfile}"
-        #aria2c -x5 https://get.quicksync.io/${syncfile} -o sync.lz4 
-        aria2c  -x5 https://get.quicksync.io/${syncfile}
-        curl -O  https://get.quicksync.io/${syncfile}.checksum
-        hash=$(curl -s https://get.quicksync.io/${syncfile}.hash)
-        # Note: this is stored on mainnet. this is not a bug
-        curl -s https://lcd.terra.dev/txs/${hash} |jq -r '.tx.value.memo'|sha512sum -c
-        ${HOME}/validator/checksum.sh ${syncfile}
-        pv ${syncfile} | lz4 -dc | tar xf -
-        # tar --use-compress-program=lz4 -xf ${syncfile}
-
-	mv ${HOME}/.terrad/data ${HOME}/.terrad/data.orig
-	ln -s /mnt/disks/data/terrad/data ${HOME}/.terrad/
-        ;;
-    "bombay-9")
+    "bombay-12")
 #	mkdir /mnt/disks/data/terrad/data
 	echo "${CHAIN_ID} no syncing available"
 	mv ${HOME}/.terra/data ${HOME}/.terra/data.orig
@@ -217,16 +197,17 @@ popd
 # everything is in place ..
 # lighting up daemons
 sudo systemctl daemon-reload
-sudo systemctl enable terrad 
+#sudo systemctl enable terrad 
 case "${CHAIN_ID}" in 
-    "bombay-9")
+    "bombay-12")
 	echo "terracli-server needs to be enabled in [api] section"
 	;;
     *)
-	sudo systemctl enable terracli-server
+	echo "terracli-server needs to be enabled in [api] section"
+#	sudo systemctl enable terracli-server
 	;;
 esac
 
 echo "Remember to hand-edit /etc/stackdriver/collectd.conf Interval for non-prod boxes"
 # and machine is ready to rock&roll.
-sudo reboot
+#sudo reboot
